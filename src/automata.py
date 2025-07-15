ca4b0877e88c8937999a736847690e5cf61162e4
Yoguti <klaus2027@gmail.com>
2025-07-15 04:36:01 -0300

    automata.py added

--- src/automata.py
+++ src/automata.py
@@ -1,8 +1,9 @@
 import random
 from abc import ABC, abstractmethod
+from typing import Optional
 
 class FastGrid:
-    def __init__(self, width, height, fill=0):
+    def __init__(self, width, height, fill: bool =False):
         self.width = width
         self.height = height
         self.data = [fill] * (width * height)
@@ -20,10 +21,9 @@ class FastGrid:
             lines.append(line)
         return '\n'.join(lines)
 
-
-class Automata:
+class Automata(ABC):
     FastGrid grid = None
-    def __init__(self, name, height, width, generations, initial_state=None):
+    def __init__(self, name: str, height: int, width: int, generations: int, initial_state: Optional[list] = None):
         self.name = name
         self.width = width
         self.height = height
@@ -31,13 +31,75 @@ class Automata:
         self.state = initial_state
 
     def create_grid(self):
-        grid = FastGrid(self.width, self.height)
+        self.grid = FastGrid(self.width, self.height)
 
     def set_initial_state(self):
+        #'Block' 'Glider' 'Random' 'Single'
+        if self.state == 'Block':
+            place_block()
+        elif self.state == 'Glider':
+            place_glider()
+        elif self.state == 'Random':
+            place_random()
+        elif self.state == 'Single':
+            place_single()
+        else:
+            raise ValueError(f"Unknown state: {self.state}")
+
+    def update_grid(self, x, y, state: bool):
+        self.grid.set(x, y, state)
+
+    def get_cell(self, x, y):
+        return self.grid.get(x,y)
+
+    def place_block(self):
+        hw = self.width // 2
+        hh = self.height // 2
+        
+        for y in range (-1,2):
+            for x in range (-1,2):
+                self.grid.set(hh + x, hw +y, True)
+    
+    def place_glider(self):
+        cx = self.width // 2
+        cy = self.height // 2
+
+        self.grid.set(cx + 1, cy,     True)
+        self.grid.set(cx + 2, cy + 1, True)
+        self.grid.set(cx,     cy + 2, True)
+        self.grid.set(cx + 1, cy + 2, True)
+        self.grid.set(cx + 2, cy + 2, True)
+      
+    def place_random(self):
+        life_max = random.randint(0, (self.width * self.height) - 1)
+
+        for life in range(life_max):
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+            self.grid.set(x, y, True)
+    
+    def place_single(self):
+        cx = self.width // 2
+        cy = self.height // 2
+        self.grid.set(cx, cy, True)
+
+    @abstractmethod
+    def run_automata(self):
         pass
 
     @abstractmethod
-    def run_automata(self)
+    def count_neighbours(self, x, y):
+        pass
+
+class Conways(Automata):
+    def __init__(self, width, height, generations, initial_state=None):
+        super().__init__('Conway', width, height, generations, initial_state)
+
+    def run_automata(self):
+        pass
+
+    def count_neighbours(self):
+        pass
 
     
 
